486d8578ebdf94e1be9fd75a8487c082828a1881
JAMP1802 <jamorochop@uce.edu.ec>
2019-11-26 12:56:49 -0500

    commit

--- server.js
+++ server.js
@@ -7,13 +7,4 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-
-router.render = function (req, res) {
-  var data = res.locals.data
-  var str = convertDataToXML(data)
-  res.send(str)
-}
-
-server.listen(port, function(){
-	console.log('JSON Server is running')
-});
+server.listen(port);

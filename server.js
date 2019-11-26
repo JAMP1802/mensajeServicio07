d06b808632226f04b7b13c85e672bdbaa7b984cc
JAMP1802 <jamorochop@uce.edu.ec>
2019-11-26 08:44:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const routerUser = jsonServer.router('usuario.json');
+const routerUser = jsonServer.routerUser('usuario.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 

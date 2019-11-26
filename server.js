415ad823113ad12288a0a4bd793696310bba7a74
JAMP1802 <jamorochop@uce.edu.ec>
2019-11-26 08:43:16 -0500

    prueba usuario

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const routerUser = jsonServer.router('usuario.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use(routerUser);
 
 server.listen(port);
